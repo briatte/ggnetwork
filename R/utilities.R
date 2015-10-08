f257024c6887fb646fb2e90dbdb1d3b2666d22db
François <briatte@gmail.com>
2015-10-08 19:27:50 +0200

    network-friendly theme

--- R/utilities.R
+++ R/utilities.R
@@ -4,3 +4,16 @@ load_pkg <- function(x) {
     stop("install the '", x, "' package to use ggnetwork")
   }
 }
+
+#' Blank theme
+#'
+#' A \code{ggplot2} theme without lines, borders, axis text or titles, suited
+#' for plotting networks.
+theme_blank <- function(base_size = 12, base_family = "") {
+  theme(line = element_blank(),
+        rect = element_blank(),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), "lines"),
+        complete = TRUE)
+}
