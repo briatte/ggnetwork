0c8d0224cc9327de6ae0d37c26feba5f1a9a6f0b
Jake Fisher <jakef@ad.isr.umich.edu>
2017-07-26 08:36:38 -0400

    Updated documentation

--- R/fortify-igraph.R
+++ R/fortify-igraph.R
@@ -2,16 +2,25 @@
 #'
 #' This function requires the \code{\link[intergraph:intergraph-package]{intergraph}}
 #' package to be installed.
-#' @param model an object of class \code{\link[igraph:igraph-package]{igraph}},
-#' which will be converted to an object of class \code{\link[network]{network}}
-#' with the \code{\link[intergraph]{asNetwork}} function before being passed to
-#' the \code{\link{fortify.network}} function.
-#' @param ... additional parameters for the \code{fortify.network} function; see
-#' \code{\link{fortify.network}}.
+#' @param model an object of class \code{\link[igraph:igraph-package]{igraph}}
+#' @param layout a function call to an 
+#' \code{\link[igraph:igraph-package]{igraph}} layout function.  See 
+#' \code{\link[igraph]{layout_}} for details.
+#' @param arrow.gap a parameter that will shorten the network edges in order to
+#' avoid overplotting edge arrows and nodes; defaults to \code{0} when the
+#' network is undirected (no edge shortening), or to \code{0.025} when the
+#' network is directed. Small values near \code{0.025} will generally achieve
+#' good results when the size of the nodes is reasonably small.
+#' @param by a character vector that matches an edge attribute, which will be
+#' used to generate a data frame that can be plotted with
+#' @param ... additional parameters for the \code{layout} argument; see
+#' \code{\link[sna]{gplot.layout}} for available options.
 #' @method fortify igraph
 #' @importFrom utils installed.packages
 #' @export
-fortify.igraph <- function(model, ...) {
+fortify.igraph <- function(model, layout = igraph::nicely(), 
+                           arrow.gap = ifelse(network::is.directed(x), 0.025, 0),
+                           by = NULL) {
 
   if ("intergraph" %in% rownames(utils::installed.packages())) {
 
