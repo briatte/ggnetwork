99982f0dbdd63fc8f667dca0482922b7212bf0ad
Jake Fisher <jakef@ad.isr.umich.edu>
2017-07-26 08:38:38 -0400

    Fixed documentation and added copy of fortify.network code

--- R/fortify-igraph.R
+++ R/fortify-igraph.R
@@ -4,7 +4,8 @@
 #' package to be installed.
 #' @param model an object of class \code{\link[igraph:igraph-package]{igraph}}
 #' @param layout a function call to an 
-#' \code{\link[igraph:igraph-package]{igraph}} layout function.  See 
+#' \code{\link[igraph:igraph-package]{igraph}} layout function, or a 2 column
+#' matrix giving the x and y coordinates for the vertices.  See 
 #' \code{\link[igraph]{layout_}} for details.
 #' @param arrow.gap a parameter that will shorten the network edges in order to
 #' avoid overplotting edge arrows and nodes; defaults to \code{0} when the
@@ -23,5 +24,94 @@ fortify.igraph <- function(model, layout = igraph::nicely(),
                            by = NULL) {
 
   
+  x = model
+  
+  # node placement
+  if (class(layout) == "matrix" &&
+      nrow(layout) == network::network.size(x) &&
+      ncol(layout) == 2) {
+    nodes = layout[, 1:2 ]
+  } else {
+    layout = paste0("gplot.layout.", layout)
+    ns <- loadNamespace("sna")
+    if (!exists(layout, envir=ns, inherits=FALSE)) {
+      stop("unsupported layout")
+    }
+    nodes = do.call( utils::getFromNamespace(layout, ns), list(x, layout.par = list(...)))
+  }
+  
+  # store coordinates
+  nodes = data.frame(nodes)
+  names(nodes) = c("x", "y")
+  
+  # rescale coordinates
+  nodes$x = scale(nodes$x, center = min(nodes$x), scale = diff(range(nodes$x)))
+  nodes$y = scale(nodes$y, center = min(nodes$y), scale = diff(range(nodes$y)))
+  
+  # import vertex attributes
+  for (y in network::list.vertex.attributes(x)) {
+    nodes = cbind(nodes, network::get.vertex.attribute(x, y))
+    names(nodes)[ncol(nodes)] = y
+  }
+  
+  # edge list
+  edges = network::as.matrix.network.edgelist(x, attrname = weights)
+  
+  # edge list (if there are duplicated rows)
+  if (nrow(edges[, 1:2]) > nrow(unique(edges[, 1:2]))) {
+    warning("duplicated edges detected")
+  }
+  
+  edges = data.frame(nodes[edges[, 1], 1:2], nodes[edges[, 2], 1:2])
+  names(edges) = c("x", "y", "xend", "yend")
+  
+  # arrow gap (thanks to @heike and @ethen8181 for their work on this issue)
+  if (arrow.gap > 0) {
+    x.length = with(edges, xend - x)
+    y.length = with(edges, yend - y)
+    arrow.gap = with(edges, arrow.gap / sqrt(x.length ^ 2 + y.length ^ 2))
+    edges = transform(
+      edges,
+      # x = x + arrow.gap * x.length,
+      # y = y + arrow.gap * y.length,
+      xend = x + (1 - arrow.gap) * x.length,
+      yend = y + (1 - arrow.gap) * y.length
+    )
+  }
+  
+  # import edge attributes
+  for (y in network::list.edge.attributes(x)) {
+    edges = cbind(edges, network::get.edge.attribute(x, y))
+    names(edges)[ncol(edges)] = y
+  }
+  
+  # merge edges and nodes data
+  edges = merge(nodes, edges, by = c("x", "y"), all = TRUE)
+  
+  # add missing columns to nodes data
+  nodes$xend = nodes$x
+  nodes$yend = nodes$y
+  names(nodes) = names(edges)[1:ncol(nodes)]
+  
+  # make nodes data of identical dimensions to edges data
+  for (y in names(edges)[(1 + ncol(nodes)):ncol(edges)]) {
+    nodes = cbind(nodes, NA)
+    names(nodes)[ncol(nodes)] = y
+  }
+  
+  # panelize nodes (for temporal networks)
+  if (!is.null(by)) {
+    nodes = lapply(sort(unique(edges[, by ])), function(x) {
+      y = nodes
+      y[, by ] = x
+      y
+    })
+    nodes = do.call(rbind, nodes)
+  }
+  
+  # return a data frame with network.size(x) + network.edgecount(x) rows,
+  # or length(unique(edges[, by ])) * network.size(x) + network.edgecount(x)
+  # rows if the nodes have been panelized
+  unique(rbind(nodes, edges[ !is.na(edges$xend), ]))
  
 }
