afdd8694509075e96275814e2f510f4ce96a6504
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-19 16:19:03 +0200

    restyle; try to fix "Undefined global functions or variables: y"

--- R/fortify-igraph.R
+++ R/fortify-igraph.R
@@ -2,8 +2,8 @@
 #'
 #' @param model an object of class \code{\link[igraph:igraph-package]{igraph}}
 #' @param data not used by this method.
-#' @param layout a function call to an 
-#' \code{\link[igraph:igraph-package]{igraph}} layout function, such as 
+#' @param layout a function call to an
+#' \code{\link[igraph:igraph-package]{igraph}} layout function, such as
 #' \code{\link[igraph]{layout_nicely}} (the default), or a 2 column matrix
 #' giving the x and y coordinates for the vertices.
 #' See \code{\link[igraph]{layout_}} for details.
@@ -19,30 +19,28 @@
 #' @method fortify igraph
 #' @importFrom utils installed.packages
 #' @export
-fortify.igraph <- function(model, data = NULL, layout = igraph::nicely(), 
+fortify.igraph <- function(model, data = NULL, layout = igraph::nicely(),
                            arrow.gap = ifelse(igraph::is.directed(x), 0.025, 0),
                            by = NULL, ...) {
+  x <- model
 
-  
-  x = model
-  
   # node placement
   if (class(layout) == "matrix" &&
-      nrow(layout) == igraph::gorder(x) &&
-      ncol(layout) == 2) {
-    nodes = layout[, 1:2 ]
+    nrow(layout) == igraph::gorder(x) &&
+    ncol(layout) == 2) {
+    nodes <- layout[, 1:2 ]
   } else {
     nodes <- igraph::layout_(x, layout, ...)
   }
-  
+
   # store coordinates
-  nodes = data.frame(nodes)
-  names(nodes) = c("x", "y")
-  
+  nodes <- data.frame(nodes)
+  names(nodes) <- c("x", "y")
+
   # rescale coordinates
   nodes$x <- scale_safely(nodes$x)
   nodes$y <- scale_safely(nodes$y)
-  
+
   # import vertex attributes
   if (length(igraph::list.vertex.attributes(x))) {
     nodes <- cbind(
@@ -56,24 +54,24 @@ fortify.igraph <- function(model, data = NULL, layout = igraph::nicely(),
     )
   }
 
-  
+
   # edge list
   edges <- igraph::as_edgelist(x, names = F)
-  
+
   # edge list (if there are duplicated rows)
   if (nrow(edges[, 1:2]) > nrow(unique(edges[, 1:2]))) {
     warning("duplicated edges detected")
   }
-  
-  edges = data.frame(nodes[edges[, 1], 1:2], nodes[edges[, 2], 1:2])
-  names(edges) = c("x", "y", "xend", "yend")
-  
+
+  edges <- data.frame(nodes[edges[, 1], 1:2], nodes[edges[, 2], 1:2])
+  names(edges) <- c("x", "y", "xend", "yend")
+
   # arrow gap (thanks to @heike and @ethen8181 for their work on this issue)
   if (arrow.gap > 0) {
-    x.length = with(edges, xend - x)
-    y.length = with(edges, yend - y)
-    arrow.gap = with(edges, arrow.gap / sqrt(x.length ^ 2 + y.length ^ 2))
-    edges = transform(
+    x.length <- edges$xend - edges$x
+    y.length <- edges$yend - edges$y
+    arrow.gap <- with(edges, arrow.gap / sqrt(x.length^2 + y.length^2))
+    edges <- transform(
       edges,
       # x = x + arrow.gap * x.length,
       # y = y + arrow.gap * y.length,
@@ -81,7 +79,7 @@ fortify.igraph <- function(model, data = NULL, layout = igraph::nicely(),
       yend = y + (1 - arrow.gap) * y.length
     )
   }
-  
+
   # import edge attributes
   if (length(igraph::list.edge.attributes(x))) {
     edges <- cbind(
@@ -91,35 +89,34 @@ fortify.igraph <- function(model, data = NULL, layout = igraph::nicely(),
         FUN = igraph::get.edge.attribute,
         graph = x,
         USE.NAMES = T
-        )
       )
+    )
   }
 
   # merge edges and nodes data
-  edges = merge(nodes, edges, by = c("x", "y"), all = TRUE)
-  
+  edges <- merge(nodes, edges, by = c("x", "y"), all = TRUE)
+
   # add missing columns to nodes data
-  nodes$xend = nodes$x
-  nodes$yend = nodes$y
-  names(nodes) = names(edges)[1:ncol(nodes)]
-  
+  nodes$xend <- nodes$x
+  nodes$yend <- nodes$y
+  names(nodes) <- names(edges)[1:ncol(nodes)]
+
   # make nodes data of identical dimensions to edges data
   missing.cols <- names(edges)[which(!(names(edges) %in% names(nodes)))]
   nodes[missing.cols] <- NA
-  
+
   # panelize nodes (for temporal networks)
   if (!is.null(by)) {
-    nodes = lapply(sort(unique(edges[, by ])), function(x) {
-      y = nodes
-      y[, by ] = x
+    nodes <- lapply(sort(unique(edges[, by ])), function(x) {
+      y <- nodes
+      y[, by ] <- x
       y
     })
-    nodes = do.call(rbind, nodes)
+    nodes <- do.call(rbind, nodes)
   }
-  
+
   # return a data frame with network.size(x) + network.edgecount(x) rows,
   # or length(unique(edges[, by ])) * network.size(x) + network.edgecount(x)
   # rows if the nodes have been panelized
   unique(rbind(nodes, edges[ !is.na(edges$xend), ]))
- 
 }
