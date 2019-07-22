f3224f11dbce8817691bc10b68887ae8c950ec1c
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-07-22 15:45:40 +0200

    indent param description

--- R/ggnetwork.R
+++ R/ggnetwork.R
@@ -3,14 +3,15 @@
 #' A wrapper for the \code{\link{fortify.network}} and
 #' \code{\link{fortify.igraph}} functions that will also try to coerce matrices
 #' and data frames to network objects.
+#'
 #' @param x an object of class \code{\link[network]{network}}, or any object
-#' that can be coerced to this class, such as an adjacency or incidence matrix,
-#' or an edge list: see \code{\link[network]{edgeset.constructors}} and
-#' \code{\link[network]{network}} for details. If the object is of class
-#' \code{\link[igraph:igraph-package]{igraph}} and the
-#' \code{\link[intergraph:intergraph-package]{intergraph}} package is installed,
-#' it will be used to convert the object: see
-#' \code{\link{fortify.igraph}} for details.
+#'   that can be coerced to this class, such as an adjacency or incidence matrix,
+#'   or an edge list: see \code{\link[network]{edgeset.constructors}} and
+#'   \code{\link[network]{network}} for details. If the object is of class
+#'   \code{\link[igraph:igraph-package]{igraph}} and the
+#'   \code{\link[intergraph:intergraph-package]{intergraph}} package is installed,
+#'   it will be used to convert the object: see
+#'   \code{\link{fortify.igraph}} for details.
 #'
 #' @param ... arguments passed to the \code{\link{fortify.network}} or \code{\link{fortify.igraph}} function.
 #'
