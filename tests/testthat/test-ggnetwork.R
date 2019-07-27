a286424b80421e3557225f9d15fca6129dab7f93
François Briatte <briatte@gmail.com>
2019-07-27 18:13:32 +0200

    add most tests for `fortify.igraph`

--- tests/testthat/test-ggnetwork.R
+++ tests/testthat/test-ggnetwork.R
@@ -3,19 +3,35 @@ test_that("ggnetwork works", {
   expect_error(ggnetwork(-999, "could not coerce"))
   expect_error(ggnetwork(emon[[1]], layout = -999, "unsupported layout"))
 
+  # with igraph
+  n <- igraph::graph_from_adjacency_matrix(as.matrix(emon[[1]]))
+  igraph::E(n)$Frequency <- network::get.edge.attribute(emon[[1]], "Frequency")
+
   # facet by edge attribute
   expect_s3_class({
     ggnetwork(emon[[1]], arrow.gap = 0.02, by = "Frequency")
+  }, class = "data.frame")
+  # with igraph
+  expect_s3_class({
+    ggnetwork(n, arrow.gap = 0.02, by = "Frequency")
   }, class = "data.frame")
 
   # user-provided layout
   expect_s3_class({
     ggnetwork(emon[[1]], layout = matrix(runif(28), ncol = 2))
   }, class = "data.frame")
+  # with igraph
+  expect_s3_class({
+    ggnetwork(n, layout = matrix(runif(28), ncol = 2))
+  }, class = "data.frame")
 
   # edge weights in layout
   expect_s3_class({
     ggnetwork(emon[[1]], layout = "kamadakawai", weights = "Frequency")
+  }, class = "data.frame")
+  # with igraph
+  expect_s3_class({
+    ggnetwork(n, layout = igraph::with_kk(weights = igraph::E(n)$Frequency))
   }, class = "data.frame")
 
   # duplicated edges warning
