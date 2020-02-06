9edb67a73878f78b4a1be7417aca2f14bf57c1cf
François Briatte <briatte@gmail.com>
2020-02-06 12:50:46 +0100

    devtools::build_vignettes()

--- vignettes/ggnetwork.R
+++ vignettes/ggnetwork.R
@@ -1,73 +1,73 @@
-## ---- echo=FALSE, message=FALSE------------------------------------------
+## ---- echo=FALSE, message=FALSE-----------------------------------------------
 library(ggplot2)
 library(ggnetwork)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  install.packages("ggnetwork")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  remotes::install_github("briatte/ggnetwork")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  install.packages("ggplot2")
 #  library(ggplot2)
 
-## ---- message=FALSE------------------------------------------------------
+## ---- message=FALSE-----------------------------------------------------------
 library(network)
 library(sna)
 n <- network(rgraph(10, tprob = 0.2), directed = FALSE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 n %v% "family" <- sample(letters[1:3], 10, replace = TRUE)
 n %v% "importance" <- sample(1:3, 10, replace = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 e <- network.edgecount(n)
 set.edge.attribute(n, "type", sample(letters[24:26], e, replace = TRUE))
 set.edge.attribute(n, "day", sample(1:3, e, replace = TRUE))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 theme_blank
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  ggnetwork(n, layout = "fruchtermanreingold", cell.jitter = 0.75)
 #  ggnetwork(n, layout = "target", niter = 100)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 head(ggnetwork(n))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 tail(ggnetwork(n))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  ggplot(n)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  ggplot(ggnetwork(n))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey50") +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey50", curvature = 0.1) +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey50") +
   geom_nodes(aes(color = family, size = importance)) +
   theme_blank()
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
 #    geom_edges(aes(color = type)) +
 #    geom_nodes(aes(color = family)) +
 #    theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(color = "black") +
   geom_nodes(color = "black", size = 8) +
@@ -75,14 +75,14 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
                 fontface = "bold") +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(color = "black") +
   geom_nodelabel(aes(color = family, label = LETTERS[ vertex.names ]),
                  fontface = "bold") +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(color = "black") +
   geom_nodelabel_repel(aes(color = family, label = LETTERS[ vertex.names ]),
@@ -90,7 +90,7 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_nodes(color = "black", size = 8) +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey75") +
   geom_nodes(color = "gold", size = 8) +
@@ -102,7 +102,7 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
         panel.background = element_rect(fill = "grey25"),
         panel.grid = element_blank())
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey75") +
   geom_nodes(color = "gold", size = 8) +
@@ -115,26 +115,26 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
         panel.background = element_rect(fill = "grey25"),
         panel.grid = element_blank())
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 data(emon)
 emon[[1]]
 
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 ggplot(emon[[1]], aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges() +
   geom_nodes(color = "tomato", size = 4) +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(emon[[1]], aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(arrow = arrow(length = unit(6, "pt"), type = "closed")) +
   geom_nodes(color = "tomato", size = 4) +
   theme_blank()
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  ggnetwork(emon[[1]], weights = "Frequency")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(ggnetwork(emon[[1]], arrow.gap = 0.04, by = "Frequency"),
        aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(arrow = arrow(length = unit(6, "pt"), type = "closed"),
@@ -143,10 +143,10 @@ ggplot(ggnetwork(emon[[1]], arrow.gap = 0.04, by = "Frequency"),
   facet_wrap(~ Frequency) +
   theme_facet()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 theme_facet
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(aes(linetype = type), color = "grey50") +
   geom_nodes(aes(x, y, color = family, size = 1.5 * importance)) +
@@ -156,7 +156,7 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   scale_size_area("importance", breaks = 1:3, max_size = 9) +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(color = "grey50", alpha = 0.5) +
   geom_nodes(aes(x, y, color = family, size = 5.5 * importance), alpha = 0.5) +
@@ -165,7 +165,7 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   guides(size = FALSE) +
   theme_blank()
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_edges(color = "grey50", alpha = 0.5) +
   geom_nodes(aes(x, y, color = family), size = 3) +
@@ -175,7 +175,7 @@ ggplot(n, aes(x = x, y = y, xend = xend, yend = yend)) +
   scale_color_brewer(palette = "Set1") +
   theme_blank()
 
-## ---- results='asis', echo=FALSE-----------------------------------------
+## ---- results='asis', echo=FALSE----------------------------------------------
 cat("Last printed on ", gsub("\\s+", " ", format(Sys.time(), "%b %e, %Y")), 
     ", using ggnetwork version ", as.character(packageVersion("ggnetwork")),
     ".", sep = "")
